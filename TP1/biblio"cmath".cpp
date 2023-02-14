85d73c5032d3fc0b3315dff7d8d33e9aa4811389
Sofitropi <124746892+Sofitropi@users.noreply.github.com>
2023-02-13 22:52:53 -0500

    Create biblio"cmath".cpp

---
+++ TP1/biblio"cmath".cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+8/02/2023 USANDO FUNCIONES (raiz) DE LA BIBLIOTECA CMATH
+
+autor:sofia carvajal 
+*******************************************************************************/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double numerito1, numerito2, numerito3;
+    
+    cout<<"ingrese 3 numeros al que desea sacarle la raiz"<<endl;
+    
+    cout<< "primer numero: "<<endl;  /*SE RECOPILAN LOS DATOS DE LOS NUMEROS*/
+    cin>> numerito1;
+    
+    cout<< "segundo numero: "<<endl;
+    cin>> numerito2;
+    
+    cout<< "tercer numero: "<<endl;
+    cin>> numerito3;
+    
+    cout<< "Resultados: "<<endl;
+    cout<<sqrt(numerito1)<<endl;
+    cout<<sqrt(numerito2)<<endl;
+    cout<<sqrt(numerito3)<<endl;
+    
+
+    
+    return 0;
+}
