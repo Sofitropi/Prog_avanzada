655feba64212b6c6bc9faa0f25cd0b7f22637d3d
Sofitropi <124746892+Sofitropi@users.noreply.github.com>
2023-02-19 18:21:39 -0500

    Update gym.cpp

--- TP1/tareas/gym.cpp
+++ TP1/tareas/gym.cpp
@@ -1,27 +1,78 @@
-//autor:sofi c
-//fecha :10/02/2023
-//para saber cuanto mide cada var
+/*poder registrar un numero de usuarios x   con y cantidad de datos en un gimnasio
+la cantidad de usuarios a registrar debe ser preguntada al principio, al final del
+programa debe de imprimirse el nombre de los usuarios registradas exitosamente  */
 
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
-int main()
-{
-    cout<<"*******Tipos fundamentales en c++***********"<<endl;
-    //size of es los bytes que tiene ese tipo de variable
-    cout<<"\t char: \t"<< sizeof(char)<<endl;  
-    cout<<"\t short: "<< sizeof(short)<<endl; 
-    cout<<"\t int: \t"<< sizeof(int)<<endl; 
-    cout<<"\t long: \t"<< sizeof(long)<<endl; 
-    cout<<"\t float: "<< sizeof(float)<<endl; 
-    cout<<"\t double: "<< sizeof(double)<<endl;   
-    cout<<"\t long double: "<< sizeof(long double)<<endl; 
+/* se crea una struct/TDA para la captura de datos del usuario */
+
+struct DatosPersonas{
+    string nombre;
+    int edad;
+    char genero;
+    string fechadeinscripcion;
+    int hijos;
+    float masamuscular;
+    string plan;
+    string hora;
+    string lesiones;
+    float altura;
+
+};
+
+
+int main() {
+    int i ;
+  int cantidad_de_personas;
+     cout<<"**************************************************"<<endl;
+     cout<<"**************************GYM********************"<<endl;
+     cout<<"**************************************************"<<endl;
     
-    cout<<"********************************************"<<endl;
+    cout<<"cantidad de personas a registar";
+    cin>> cantidad_de_personas;
     
+    DatosPersonales persona[cantidad_de_personas];
     
+    cout<<"ingreso de datos"<<endl;
     
-    return 0;
-}
+    for(i=0;i<cantidad_de_personas;i++){
+  
+        cout<<"ingrese nombre: "<<endl;
+        cin>> persona[i].nombre;
+        
+        cout<<"ingrese edad: "<<endl;
+        cin>>persona[i].edad;
+        
+        cout<<"ingrese genero: "<<endl;
+        cin>>persona[i].genero;
+        
+        cout<<"ingrese fecha de inscripcion: "<<endl;
+        cin>>persona[i].fechadeinscripcion;
+            
+        cout<<"ingrese numero de hijos: "<<endl;
+        cin>>persona[i].hijos;
+            
+        cout<<"ingrese masa muscular: "<<endl;
+        cin>>persona[i].masamuscular;
+            
+        cout<<"ingrese plan a adquirir: "<<endl;
+        cin>>persona[i].plan;
+            
+        cout<<"ingrese franja horaria: "<<endl;
+        cin>>persona[i].franjahoraria;
+        
+        cout<<"ingrese lesiones: "<<endl;
+        cin>>persona[i].lesiones;
+            
+        cout<<"ingrese su altura: "<<endl;
+        cin>>persona[i].altura;
+       
+   }
+   for(int j=0;j<cantidad_de_personas;j++)
+   {
+   
+   cout<< "usuarios inscritos correctamente"<<endl;
+   }
+ 
+
